11fe905587dc78fe20c98a84c9c3e35ba0a2f930
just X // Developer OxyHub <76210400+OxyHub-Team@users.noreply.github.com>
2024-10-23 20:16:59 +0300

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,63 +1,49 @@
-_G.AutoMarket = true
-_G.AutoMarket2 = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OxyHub-Team/test/refs/heads/main/test.lua"))()
 
-function autobuymarkets2()
-    local merchantids = {6,5,4,3,2,1}
-    for i = 1, 5 do
-        for _, id in ipairs(merchantIds) do
-            local args = {
-                [1] = "StandardMerchant",
-                [2] = id
-            }
+_G.CheckPets = true
 
-            game:GetService("ReplicatedStorage").Network.CustomMerchants_Purchase:InvokeServer(unpack(args))
-        end
-    end
+function trackpets()
+    local eggHatchEvent = game:GetService("ReplicatedStorage").Network.Pets_Restore
+    eggHatchEvent.OnClientEvent:Connect(function(petName)
+        print("Выпал питомец: " .. petName)
+    end)
 end
-
-function automarket()
-    while _G.AutoMarket = true do
-        autobuymarkets2()
-        wait(600)
-    end
-end
-
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "Rayfield Example Window",
-    LoadingTitle = "Rayfield Interface Suite",
-    LoadingSubtitle = "by Sirius",
-    ConfigurationSaving = {
-       Enabled = true,
-       FolderName = nil, -- Create a custom folder for your hub/game
-       FileName = "Big Hub"
-    },
-    Discord = {
-       Enabled = false,
-       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-    },
-    KeySystem = false, -- Set this to true to use our key system
-    KeySettings = {
-       Title = "Untitled",
-       Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
-       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-    }
- })
+   Name = "Rayfield Example Window",
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
 
- local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
-
- local Toggle = Tab:CreateToggle({
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+local Toggle = Tab:CreateToggle({
     Name = "Toggle Example",
     CurrentValue = false,
-    Flag = "Toggle15", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
     Callback = function(Value)
-        _G.AutoMarket = Value
-        automarket()
+        while _G.CheckPets = true do
+            trackpets()
+            wait(5)
+        end
     end,
  })
